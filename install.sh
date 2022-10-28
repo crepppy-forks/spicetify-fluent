20bb632c17e1fa10ee9ebd7541424fe5cffaa48b
Jack Chapman <chapmanjack0777@gmail.com>
2022-10-28 21:28:35 +0100

    Fix install.sh for some shells

--- install.sh
+++ install.sh
@@ -6,31 +6,34 @@ echo "Beginning installation of spicetify-fluent"
 echo "https://github.com/williamckha/spicetify-fluent"
 
 # Give time for user to cancel via CTRL+C
-sleep 3s
+#sleep 3s
 
 # Check if ~\.spicetify-cli\Themes\Fluent directory exists
-spicePath=dirname "$(spicetify -c)"
-themePath=dirname "$spicePath/Themes/Fluent"
+spicePath=$(dirname "$(spicetify -c)")
+themePath="$spicePath/Themes/Fluent"
 if [ ! -d $themePath ]; then
     echo "Creating Fluent theme folder..."
     mkdir -p $themePath
 else
     # Remove pre-existing files, only keep the newest files
-    rm -rfv "$themePath/*"
+    echo "Removing previous install files..."
+    rm -rf "$themePath"/*
 fi
 
 # Download latest master
 zipUri="https://github.com/williamckha/spicetify-fluent/archive/refs/heads/master.zip"
 zipSavePath="$themePath/fluent-master.zip"
+echo $zipUri
+echo $zipSavePath
 echo "Downloading spicetify-fluent latest master..."
-curl --fail --location --progress-bar --output "$zipUri" "$zipSavePath"
+curl "$zipUri" -fL#o "$zipSavePath"
 
 
 # Extract theme from .zip file
 echo "Extracting..."
 unzip -d "$themePath" -o "$zipSavePath"
-mv "$themePath/spicetify-fluent-master/*" $themePath
-rmdir "$themePath/spicetify-fluent-master"
+mv "$themePath/spicetify-fluent-master"/* $themePath
+rm -rf "$themePath/spicetify-fluent-master"
 
 # Delete .zip file
 echo "Deleting zip file..."
@@ -53,13 +56,15 @@ echo "+ Configured Fluent theme"
 
 # Patch the xpui.js for sidebar fixes
 # credit: https://github.com/JulienMaille/dribbblish-dynamic-theme/blob/main/install.sh
+configPath="$spicePath/config-xpui.ini"
+echo $configPath
 PATCH='[Patch]
 xpui.js_find_8008 = ,(\\w+=)32,
 xpui.js_repl_8008 = ,\${1}58,'
-if cat config-xpui.ini | grep -o '\[Patch\]'; then
-    perl -i -0777 -pe "s/\[Patch\].*?($|(\r*\n){2})/$PATCH\n\n/s" config-xpui.ini
+if cat $configPath | grep -o '\[Patch\]'; then
+    perl -i -0777 -pe "s/\[Patch\].*?($|(\r*\n){2})/$PATCH\n\n/s" "$configPath"
 else
-    echo -e "\n$PATCH" >> config-xpui.ini
+    echo -e "\n$PATCH" >> "$config-xpui.ini"
 fi
 echo "+ Patched xpui.js for Sidebar fixes"
 
